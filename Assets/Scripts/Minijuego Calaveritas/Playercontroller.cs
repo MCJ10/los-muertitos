a560c20a5423097e09b1fb03199c0439012d26f9
Maria jose castro jaramillo <70542373+diesistmaria@users.noreply.github.com>
2021-10-20 13:25:52 -0500

    intento

--- Assets/Scripts/Minijuego Calaveritas/Playercontroller.cs
+++ Assets/Scripts/Minijuego Calaveritas/Playercontroller.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    public float Speed;
+    public int life = 3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveByHover();
+
+
     }
+
+    void moveByHover()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);// get the hover position
+        direction = (mousePosition - transform.position).normalized; // direction from walking to point position 
+        rb.velocity = new Vector2(direction.x * Speed, 0); // rigibody gets the velocity by multiply the speed
+
+
+    }
+
+
 }
