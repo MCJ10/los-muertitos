ce8109f21008c70372902a5bd221d97f1a837f2c
TheObands1 <juansebastian3080@hotmail.com>
2021-10-20 22:59:31 -0500

    Finished Prototype of Puzzle Game

--- Assets/Scripts/PuzzleScripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleScripts/PuzzleManager.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PuzzleManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private int PointsToWin;
     private int CurrentPoints;
+    private bool SceneWasLoaded;
     public GameObject myPuzzlePieces;
 
     void Start()
     {
         PointsToWin = myPuzzlePieces.transform.childCount;
+        SceneWasLoaded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(CurrentPoints >= PointsToWin)
+        if(CurrentPoints >= PointsToWin && !SceneWasLoaded)
         {
-            //Tell Scene Manager that it won
+            SceneWasLoaded = true;
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
         }
         
     }
@@ -27,5 +31,10 @@ public class PuzzleManager : MonoBehaviour
     public void AddPoint()
     {
         CurrentPoints++;
+    }
+
+    public void GoBackToMainGame()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
     }
 }
